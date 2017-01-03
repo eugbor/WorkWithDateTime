aad397b6adce35b60e9b156bdc69b117b6cea016
eugbor <eugeneborodin@mail.ru>
2017-01-04 01:14:28 +0300

    init

---
+++ WorkWithDateTime/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+//Вывести на экран текущее название дня недели, название месяца и свое имя. Каждое слово должно быть в отдельной строке.
+//
+namespace WorkWithDateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dayValue = DateTime.Now;
+            Console.WriteLine(dayValue.DayOfWeek);
+            CultureInfo culInf = new CultureInfo("ru-RU");
+            string monthNames = culInf.DateTimeFormat.GetMonthName(dayValue.Month);
+            Console.WriteLine(monthNames);
+            Console.WriteLine("Basil");
+            Console.ReadLine();
+        }
+    }
+}
